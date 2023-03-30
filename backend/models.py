9930f823d844c064cb916b25dee79e3d2100c580
syou <dbtnwl369@gmail.com>
2023-03-29 19:24:52 -0700

    modified

--- backend/models.py
+++ backend/models.py
@@ -5,9 +5,9 @@ import json
 from settings import DB_NAME, DB_USER, DB_PASSWORD
 
 
-database_name = DB_NAME
-database_path = 'postgresql://{}:{}@{}/{}'.format(
-    DB_USER, DB_PASSWORD, 'localhost:5432', database_name)
+# database_name = DB_NAME
+# database_path = 'postgresql://{}:{}@{}/{}'.format(
+#     DB_USER, DB_PASSWORD, 'localhost:5432', database_name)
 
 # database_path = config.SQLALCHEMY_DATABASE_URI
 
@@ -19,9 +19,32 @@ setup_db(app)
 """
 
 
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# def setup_db(app, database_path=database_path):
+#     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+#     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#     db.app = app
+#     db.init_app(app)
+#     db.create_all()
+
+
+def setup_db(app):
+    ENV = 'prod'
+
+    if ENV == 'dev':
+        database_name = DB_NAME
+        database_path = 'postgresql://{}:{}@{}/{}'.format(
+            DB_USER, DB_PASSWORD, 'localhost:5432', database_name)
+
+        app.debug = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+        app.config['SECRET_KEY'] = os.urandom(32)
+    else:
+        app.debug = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+        app.config['SECRET_KEY'] = os.urandom(32)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.app = app
     db.init_app(app)
     db.create_all()
