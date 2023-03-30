210d0cfb9e07a60a50639b264d56eb928b2f3738
syou <dbtnwl369@gmail.com>
2023-03-30 00:14:30 -0700

    build folder

---
+++ frontend/build/precache-manifest.109a18a3fdd4e4f2e06d7eae0ee48c9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e5448c8341b1cc0995ac8015c70356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1130f5856e4c28e4aa1",
+    "url": "/static/css/main.e8146a77.chunk.css"
+  },
+  {
+    "revision": "1cf8af73d066c84e123b",
+    "url": "/static/js/2.966b5438.chunk.js"
+  },
+  {
+    "revision": "e1130f5856e4c28e4aa1",
+    "url": "/static/js/main.8e391929.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
