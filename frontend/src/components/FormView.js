3f25553d0118d9387c49c93428d13fd8720a75de
syou <dbtnwl369@gmail.com>
2023-04-13 10:05:39 -0700

    for deploy

--- frontend/src/components/FormView.js
+++ frontend/src/components/FormView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
+// import $ from 'jquery';
 import '../stylesheets/FormView.css';
 
 class FormView extends Component {
--- frontend/src/components/QuizView.js
+++ frontend/src/components/QuizView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
+// import $ from 'jquery';
 import '../stylesheets/QuizView.css';
 
 const questionsPerPlay = 5;
